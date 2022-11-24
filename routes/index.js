a2c903c1d0016f6ff705866529f2d3a858bea42b
Arman Hamzehlou <arman.hamzehlou@ryerson.ca>
2022-11-24 15:51:13 -0500

    adding headers

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   res.render('index', { title: 'Express' });
 });
 
